ac1b3544b1dfc4c7f77ef6c6ec38c9a67198d171
yanggeorge <alenym@qq.com>
2025-01-23 21:10:09 +0800

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1 className="text-4xl text-blue-500">Vite Project and Tailwind css</h1>
+    <body className="flex flex-col min-h-dvh bg-white dark:bg-gray-950 text-black ModelsPage" cz-shortcut-listen="true">
+      <h1 className="text-4xl text-blue-500">Vite Project and Tailwind css</h1>
+    </body>
   );
 }
 export default App;
