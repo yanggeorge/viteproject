24bb0ce7b1108851658c5d43caa080129567358c
yanggeorge <alenym@qq.com>
2025-02-24 10:59:04 +0800

    index

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
+import Index from "./pages";
+
 function App() {
-  return (
-    <body className="flex flex-col min-h-dvh bg-white dark:bg-gray-950 text-black ModelsPage" cz-shortcut-listen="true">
-      <h1 className="text-4xl text-blue-500">Vite Project and Tailwind css</h1>
-    </body>
-  );
+  return <Index />;
 }
 export default App;
