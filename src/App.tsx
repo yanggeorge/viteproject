42ac9ddace4b19d196c065e0a591444ff0492540
yanggeorge <alenym@qq.com>
2025-02-24 19:27:24 +0800

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
-import Index from "./pages";
+import { Link, RouteObject, useRoutes } from 'react-router-dom';
+import Index from './pages';
+
+function Home() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/index">Index</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>It looks like you're lost...</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+const routes: RouteObject[] = [
+  { path: '/', element: <Home /> },
+  { path: '/index', element: <Index /> },
+  { path: '*', element: <NoMatch /> },
+];
 
 function App() {
-  return <Index />;
+  return useRoutes(routes);
 }
 export default App;
