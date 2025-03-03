4c09d834b4c5af7111d33ef6ce19266a2e6b365b
yanggeorge <alenym@qq.com>
2025-03-03 11:34:24 +0800

    antd layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { StyleProvider } from '@ant-design/cssinjs'; // Replace 'some-style-library' with the actual library name
+import { ConfigProvider } from 'antd';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <StyleProvider layer>
+        <ConfigProvider>
+          <App />
+        </ConfigProvider>
+      </StyleProvider>
     </BrowserRouter>
   </StrictMode>,
 );
