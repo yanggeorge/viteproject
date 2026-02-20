2f3302b7d12464d173e445d7c618c9e4df063fac
yanggeorge <alenym@qq.com>
2026-02-20 18:02:47 +0800

    ref example

--- src/components/examples/RefExample.tsx
+++ src/components/examples/RefExample.tsx
@@ -1,5 +1,9 @@
 import { useState, useRef } from 'react';
 
+// This example uses a combination of state and refs.
+// Both startTime and now are state variables because they are used for rendering.
+// But we also need to hold an interval ID so that we can stop the interval on button press.
+// Since the interval ID is not used for rendering, it’s appropriate to keep it in a ref, and manually update it.
 const RefExample = () => {
   const [startTime, setStartTime] = useState<number | null>(null);
   const [now, setNow] = useState<number | null>(null);
